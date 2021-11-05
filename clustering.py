b2ad238d5bcac2fe43fd4d39461285e713d8e736
Gabriel Clinger <55040675+clingergab@users.noreply.github.com>
2021-11-05 15:48:01 -0400

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -8,22 +8,17 @@ class clustering:
     
     def __init__(self):
         self.image = Image.open(sys.argv[1]).convert('RGB')
-        #print(self.image)
-
         self.k = int(sys.argv[2])
     
     def KMeans(self):
         image = np.asarray(self.image)
         pixelVal = image.reshape((-1, 3))
-        #pixelVal = np.float32(pixelVal)
-        #print(pixelVal)
-        #print(image)
+
         kmeans = KMeans(n_clusters=self.k, random_state=0).fit(pixelVal)
         
         centers = np.uint8(kmeans.cluster_centers_)
         labels = kmeans.labels_.flatten()
-        #print(centers)
-        #print(labels)
+
         segImg = centers[labels.flatten()]
         segImg = segImg.reshape(image.shape)
         im = Image.fromarray(segImg)
@@ -33,7 +28,6 @@ class clustering:
 
 def main():
     cluster = clustering()
-    #cluster.image.show()
     cluster.KMeans()
 
 
